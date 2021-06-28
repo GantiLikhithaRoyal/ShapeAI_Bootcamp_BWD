0fcc71ad36b20cb21914a3a030cb1dba8406411e
GantiLikhithaRoyal <86588544+GantiLikhithaRoyal@users.noreply.github.com>
2021-06-28 11:31:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Likhitha from "./Info";
+import Cheery from "./Info2";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Likhitha />
+      <Likhitha />
+      <Likhitha />
+      <Cheery />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cheery() {
+  return (
+    <div className="note">
+      <h1>Im learning Js </h1>
+      <p>A basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Cheery;
